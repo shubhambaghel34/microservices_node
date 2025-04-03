20ef620612342e3659d7f5ba4f7f2b3a90d0c13c
shuhambaghel34 <shubhambaghel@gmail.com>
2025-04-03 10:45:24 +0530

    updated libs

--- userservice/src/server.js
+++ userservice/src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const amqp = require("amqplib");
 
